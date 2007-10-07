4d1e39942bb40c4ede41e2330320245dad9fc3f6
Dan <devnull@localhost>
2007-10-06 20:36:40 -0400

    Rebrand as 1.1.1; everything should now be bumped to "unstable" status

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 
 /*
  * Enano - an open-source CMS capable of wiki functions, Drupal-like sidebar blocks, and everything in between
- * Version 1.0.2 (Coblynau)
+ * Version 1.1.1
  * Copyright (C) 2006-2007 Dan Fuhry
  * constants.php - important defines used Enano-wide
  *
